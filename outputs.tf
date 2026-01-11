c93d85c11f187ae22a8201df61c05214cebc96af
Daniel Allen <dan@aeternagroup.net>
2026-01-11 15:30:46 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "iotcentral_application_network_rule_sets" {
+  description = "All iotcentral_application_network_rule_set resources"
+  value       = azurerm_iotcentral_application_network_rule_set.iotcentral_application_network_rule_sets
+}
+
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "iotcentral_application_network_rule_sets" {
+  description = <<EOT
+Map of iotcentral_application_network_rule_sets, attributes below
+Required:
+    - iotcentral_application_id
+Optional:
+    - apply_to_device
+    - default_action
+    - ip_rule (block):
+        - ip_mask (required)
+        - name (required)
+EOT
+
+  type = map(object({
+    iotcentral_application_id = string
+    apply_to_device           = optional(bool, true)
+    default_action            = optional(string, "Deny")
+    ip_rule = optional(object({
+      ip_mask = string
+      name    = string
+    }))
+  }))
+}
+
