24923313e072f7fa069a1810d8e8cc77992a84d7
Daniel Allen <dan@aeternagroup.net>
2026-02-11 23:53:10 +0000

    Update to schema version v4.59.0

--- variables.tf
+++ variables.tf
@@ -13,8 +13,8 @@ EOT
 
   type = map(object({
     iotcentral_application_id = string
-    apply_to_device           = optional(bool, true)
-    default_action            = optional(string, "Deny")
+    apply_to_device           = optional(bool)   # Default: true
+    default_action            = optional(string) # Default: "Deny"
     ip_rule = optional(object({
       ip_mask = string
       name    = string
